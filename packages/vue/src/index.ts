cb8ce35254759710ed56fd5722aca4881934fd26
krisnayogantaraa <kyogantara89@gmail.com>
2026-06-11 11:38:25 +0700

    feat: update package versions to 0.2.2 and remove unused exports from Vue and React components

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -7,15 +7,6 @@ export * from './components/typography';
 // Actions
 export * from './components/actions';
 
-// Forms
-export * from './components/forms';
-
-// Data Display
-export * from './components/data-display';
-
-// Feedback
-export * from './components/feedback';
-
 // Navigation
 export * from './components/navigation';
 
