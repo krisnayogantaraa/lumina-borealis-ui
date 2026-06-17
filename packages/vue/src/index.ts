18c25bc31c301c2664cce6e84326279ac86520c4
krisnayogantaraa <kyogantara89@gmail.com>
2026-06-17 14:01:07 +0700

    feat: add data display exports to main index file

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -10,6 +10,9 @@ export * from './components/actions';
 // Navigation
 export * from './components/navigation';
 
+// Data Display
+export * from './components/data-display';
+
 // Core & Config
 export * from './config';
 export { default as LumaIcon } from './components/icon/Icon.vue';
